dfb983f85a1bed56dc3a6592698b0b70bdd9f88e
bencrow11 <bencrow11@gmail.com>
2023-01-31 20:34:33 +0000

    Refactored pbtag command code and added a check for player. Now allows command to be run from console.

--- src/main/java/com/bencrow11/betterbreeding/CommandPokebuilder.java
+++ src/main/java/com/bencrow11/betterbreeding/CommandPokebuilder.java
@@ -9,6 +9,7 @@ import com.pixelmonmod.pixelmon.comm.CommandChatHandler;
 import com.pixelmonmod.pixelmon.command.PixelCommand;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.CommandSource;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.fml.server.ServerLifecycleHooks;
@@ -35,33 +36,39 @@ public class CommandPokebuilder extends PixelCommand {
 
 	@Override
 	public void execute(CommandSource sender, String[] args) throws CommandException, CommandSyntaxException {
-		// Checks for permission to use the command
-		if (!PermissionAPI.hasPermission(sender.asPlayer(), "pokebuilder.admin")) {
-			CommandChatHandler.sendChat(sender, TextFormatting.RED + "You do not have permission for this command!");
-		} else {
-			// checks arg amount is correct
+
+		// checks for a player running the command
+		if (sender.getEntity() != null) {
+			// Checks the player has the permission
+			if (!PermissionAPI.hasPermission(sender.asPlayer(), "pokebuilder.admin")) {
+				CommandChatHandler.sendChat(sender, TextFormatting.RED + "You do not have permission for this command!");
+				return;
+			}
+
+			// Checks the arg lenth is correct
 			if (args.length != 2) {
 				CommandChatHandler.sendChat(sender, TextFormatting.RED + "Usage: /pbtag <player> <slot>");
-			} else {
-				try {
-					// gets the player given
-					ServerPlayerEntity player =
-							ServerLifecycleHooks.getCurrentServer().getPlayerList().getPlayerByUsername(args[0]);
-
-					// gets the slot from the args
-					int slot = Integer.parseInt(args[1]);
-
-					assert player != null;
-					PartyStorage storage = StorageProxy.getParty(player.getUniqueID());
-
-					// adds the tag pokebuilder tag to the pokemon in the specified slot
-					Objects.requireNonNull(storage.get(slot - 1)).addFlag("Pokebuilder");
-					Objects.requireNonNull(storage.get(slot - 1)).addFlag("unbreedable");
-					Objects.requireNonNull(storage.get(slot - 1)).getPersistentData().remove("currentOwner");
-				} catch (Exception error) {
-					CommandChatHandler.sendChat(sender, TextFormatting.RED + "Usage: /pbtag <player> <slot>");
-				}
+				return;
 			}
+		}
+
+		try {
+			// gets the player given
+			ServerPlayerEntity player =
+					ServerLifecycleHooks.getCurrentServer().getPlayerList().getPlayerByUsername(args[0]);
+
+			// gets the slot from the args
+			int slot = Integer.parseInt(args[1]);
+
+			assert player != null;
+			PartyStorage storage = StorageProxy.getParty(player.getUniqueID());
+
+			// adds the tag pokebuilder tag to the pokemon in the specified slot
+			Objects.requireNonNull(storage.get(slot - 1)).addFlag("Pokebuilder");
+			Objects.requireNonNull(storage.get(slot - 1)).addFlag("unbreedable");
+			Objects.requireNonNull(storage.get(slot - 1)).getPersistentData().remove("currentOwner");
+		} catch (Exception error) {
+			CommandChatHandler.sendChat(sender, TextFormatting.RED + "Usage: /pbtag <player> <slot>");
 		}
 	}
 }
